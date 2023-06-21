ea428b65525c7646254099d5f84796e5764ec570
William Santana <williamandrade1058@gmail.com>
2023-06-21 19:16:07 -0300

    Create aula011.py

---
+++ modulo1/aula011.py
@@ -0,0 +1,7 @@
+#Desafio 011
+largura = float(input("Informe a largura: "))
+altura = float(input("Informe a altura: "))
+area = largura * altura
+print(f"A sua parede tem dimensão {largura}x{altura} e sua área é de {area}m.")
+tinta = area / 2
+print(f"Você precisa de {tinta}l para pintar a parede.")
